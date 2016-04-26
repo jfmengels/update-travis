536910c51cd80af8e77157e67d044179204e9abf
Jeroen Engels <jfm.engels@gmail.com>
2016-04-26 23:08:03 +0200

    Working CLI

--- cli.js
+++ cli.js
@@ -1,20 +1,44 @@
 #!/usr/bin/env node
+
 'use strict';
+var fs = require('fs');
+var path = require('path');
 var meow = require('meow');
-var updateTravis = require('./');
+var yaml = require('js-yaml');
+var inquirer = require('inquirer');
 
 var cli = meow([
 	'Usage',
 	'  $ update-travis [input]',
 	'',
 	'Options',
-	'  --foo  Lorem ipsum. [Default: false]',
+	'  --versions Specify the versions you want to support (separated by a `,`)',
 	'',
 	'Examples',
 	'  $ update-travis',
-	'  unicorns & rainbows',
-	'  $ update-travis ponies',
-	'  ponies & rainbows'
+	'  $ update-travis --versions 6,5,4,0.10',
+	'  $ update-travis --versions stable,0.12'
 ]);
 
-console.log(updateTravis(cli.input[0] || 'unicorns'));
+var filepath = path.join(process.cwd(), '.travis.yml');
+var doc = yaml.safeLoad(fs.readFileSync(filepath, 'utf8'));
+
+var questions = [{
+	type: 'string',
+	name: 'versions',
+	message: 'What versions do you wish to support? (Separate them by `,`)',
+	default: cli.flags.versions || doc.node_js.join(','),
+	when: !cli.flags.versions
+}];
+
+inquirer.prompt(questions)
+.then(function (answers) {
+	var versions = answers.versions || cli.flags.versions;
+	if (!versions.length) {
+		return;
+	}
+	doc.node_js = versions.split(','); // eslint-disable-line camelcase
+	fs.writeFileSync(filepath, yaml.dump(doc), 'utf8');
+	console.log('For migrating your next project, run:');
+	console.log('  update-travis --versions ' + versions);
+});
